4ffeb49d15fcbd6f5813a5f068b36ab1b582b7c7
Milan Pollé <milan.polle@ordina.nl>
2024-03-20 21:46:40 +0100

    Added a scuffed looking controller type setting. Start of controller selection logic.

--- VIC20.sv
+++ VIC20.sv
@@ -191,7 +191,7 @@ assign HDMI_FREEZE = 0;
 // 0         1         2         3          4         5         6
 // 01234567890123456789012345678901 23456789012345678901234567890123
 // 0123456789ABCDEFGHIJKLMNOPQRSTUV 0123456789ABCDEFGHIJKLMNOPQRSTUV
-// X XXX XXXXXXXXXXXXXXXXXXXXXXXXX
+// X XXX XXXXXXXXXXXXXXXXXXXXXXXXX  xx
 
 `include "build_id.v" 
 parameter CONF_STR = {
@@ -230,6 +230,7 @@ parameter CONF_STR = {
 	"FC6,ROM,Load Kernal;",
 	"-;",
 	"OU,Swap paddles,No,Yes;",
+	"o01, Controller,Joystick,Paddle,Analog", // 33:32
 	"-;",
 	"R0,Reset;",
 	"RR,Reset & Detach Cartridge;",
@@ -330,7 +331,7 @@ end
 
 /////////////////  HPS  ///////////////////////////
 
-wire [31:0] status;
+wire [63:0] status;
 wire  [1:0] buttons;
 
 wire  [7:0] pd1,pd2;
@@ -599,7 +600,13 @@ wire [7:0] mc_data = mc_nvram_sel ? mc_nvram_out : sdram_out;
 
 ///////////////////////////////////////////////////
 
-wire [15:0] joy = joya;// | joyb;
+wire [1:0] port_type = status[33:32];
+wire [15:0] joy = port_type ? joya : joya|joyb;
+wire [1:0] paddle_buttons =        {status[30] ? joya[5] : joyb[5], status[30] ? joyb[5] : joya[5]};
+wire [1:0] analog_paddle_buttons = {status[30] ? joyb[5] : joyb[4], status[30] ? joyb[4] : joyb[5]};
+wire [1:0] joy_horizontal = port_type ? (port_type == 1 ? paddle_buttons : analog_paddle_buttons) : {joy[0], joy[1]}; 
+wire [7:0] paddle_a = port_type == 1 ? (status[30] ? pd2 : pd1) : status[30] ? joya_1[7:0]: joya_1[15:8];
+wire [7:0] paddle_b = port_type == 1 ? (status[30] ? pd1 : pd2) : status[30] ? joya_1[15:8]: joya_1[7:0];
 
 reg [10:0] v20_key;
 always @(posedge clk_sys) begin
@@ -625,7 +632,7 @@ VIC20 VIC20
 	.data_o(v20_iec_data_o),
 	.clk_i(c1541_iec_clk_o & ext_iec_clk),
 	.data_i(c1541_iec_data_o & ext_iec_data),
-
+	
 	.i_joy(~{joy[0] | (status[30] ? joyb[5] : joyb[4]),joy[1] | (status[30] ? joyb[4] : joyb[5]),joy[2],joy[3]}),
 	//.i_joy(~{joy[0] | (status[30] ? joya[5] : joyb[5]),joy[1] | (status[30] ? joyb[5] : joya[5]),joy[2],joy[3]}),
 	.i_fire(~joy[4]),
